64e958ce05c2dcea07d8842c9d193af5c00b85d7
David R. Poindexter III <drpoindexter@gmail.com>
2016-01-09 14:43:32 -0500

    Basic render and props handling

--- App.js
+++ App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
 	render() {
-		return <div>Hello World!</div>
+		let txt = this.props.txt;
+		return <h1>{txt}</h1>
 	}
 }
 
-export default App;
+App.propTypes = {
+	txt: React.PropTypes.string,
+	cat: React.PropTypes.number.isRequired
+};
+
+App.defaultProps = {
+	txt: 'default text here'
+};
+
+ReactDOM.render(
+	<App cat={3} />,
+	document.getElementById('app')
+);
