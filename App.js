5664babdf5706c9ca6cfbdbbfa3db405c2d15f84
David R. Poindexter III <drpoindexter@gmail.com>
2016-01-10 11:31:15 -0500

    Use refs to access child components

--- App.js
+++ App.js
@@ -1,34 +1,48 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
 	constructor(){
 		super();
-		this.state = { txt: ''};
+		this.state = {
+			red: 0,
+			green: 0,
+			blue: 0
+		};
 		this.update = this.update.bind(this);
 	}
 	update(e){
-		this.setState({txt: e.target.value});
+		this.setState({
+			red: ReactDOM.findDOMNode(this.refs.red.refs.input).value,
+			green: ReactDOM.findDOMNode(this.refs.green.refs.input).value,
+			blue: ReactDOM.findDOMNode(this.refs.blue.refs.input).value
+		});
 	}
 	render() {
 		return(
 			<div>
-				<Widget txt={this.state.txt} update={this.update} />
-				<Widget txt={this.state.txt} update={this.update} />
-				<Widget txt={this.state.txt} update={this.update} />
-				<Widget txt={this.state.txt} update={this.update} />
-				<Widget txt={this.state.txt} update={this.update} />
+				<Slider ref="red" update={this.update} />
+				{this.state.red}
+				<Slider ref="green" update={this.update} />
+				{this.state.green}
+				<Slider ref="blue" update={this.update} />
+				{this.state.blue}
 			</div>
 		)
 	}
 }
 
-const Widget = (props) => {
-	return (
-		<div>
-			<input type="text" onChange={props.update}/>
-			<h1>{props.txt}</h1>
-		</div>
-	)
+class Slider extends React.Component {
+	render() {
+		return (
+			<div>
+				<input ref="input" type="range"
+					min="0"
+					max="255"
+					onChange={this.props.update}/>
+			</div>
+		);
+	}
 };
 
 export default App;
