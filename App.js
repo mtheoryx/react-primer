4b6d113fb86074916e3eab34f26f5e3d1335a124
David R. Poindexter III <drpoindexter@gmail.com>
2016-01-16 14:58:55 -0500

    Composing components, RGB color picker
    * Three components for RGB value 0 to 255
    * Color preview area for selected color values

--- App.js
+++ App.js
@@ -1,52 +1,97 @@
 import React from 'react';
-
-let Mixin = InnerComponent => class extends React.Component {
+import ReactDOM from 'react-dom';
+class App extends React.Component {
   constructor(){
     super();
+    this.state = {
+      red: 0,
+      green: 0,
+      blue: 0
+    };
     this.update = this.update.bind(this);
-    this.state = {val: 0}
   }
   update(){
-    this.setState({val: this.state.val + 1})
+    this.setState({
+      red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
+      green: ReactDOM.findDOMNode(this.refs.green.refs.inp).value,
+      blue: ReactDOM.findDOMNode(this.refs.blue.refs.inp).value
+    });
   }
-  componentWillMount(){
-    console.log('will mount')
-  }
-  render(){
-    return <InnerComponent
-      update={this.update}
-      {...this.state}
-      {...this.props} />
-  }
-  componentDidMount(){
-    console.log('mounted')
-  }
-}
-
-const Button = (props) => <button
-                            onClick={props.update}>
-                            {props.txt} - {props.val}
-                          </button>
-
-const Label = (props) => <label
-                            onMouseMove={props.update}>
-                            {props.txt} - {props.val}
-                          </label>
-
-let ButtonMixed = Mixin(Button)
-let LabelMixed = Mixin(Label)
-
-class App extends React.Component {
-
   render(){
     return (
       <div>
-        <ButtonMixed txt="Button" />
-        <LabelMixed txt="Label" />
+        <NumInput
+          ref="red"
+          min={0}
+          max={255}
+          step={1}
+          val={+this.state.red}
+          label="Red"
+          update={this.update} />
+        <br/>
+        <NumInput
+          ref="green"
+          min={0}
+          max={255}
+          step={1}
+          val={+this.state.green}
+          label="Green"
+          update={this.update} />
+        <br/>
+        <NumInput
+          ref="blue"
+          min={0}
+          max={255}
+          step={1}
+          val={+this.state.blue}
+          label="Blue"
+          update={this.update} />
+        <div style={{
+                    height:100,
+                    width:100,
+                    backgroundColor:`rgba(${this.state.red}, ${this.state.green}, ${this.state.blue}, 1)`}}></div>
       </div>
     );
   }
-
 }
+
+class NumInput extends React.Component {
+  render(){
+    let label = this.props.label !== '' ?
+      <label>{this.props.label} -  {this.props.val}</label> : '';
+    return (
+        <div>
+        <input ref="inp"
+          type={this.props.type}
+          min={this.props.min}
+          max={this.props.max}
+          step={this.props.step}
+          defaultValue={this.props.val}
+          onChange={this.props.update} />
+          {label}
+        </div>
+    );
+  }
+}
+
+NumInput.propTypes = {
+  min: React.PropTypes.number,
+  max: React.PropTypes.number,
+  step: React.PropTypes.number,
+  val: React.PropTypes.number,
+  label: React.PropTypes.string,
+  update: React.PropTypes.func.isRequired,
+  type: React.PropTypes.oneOf(['number', 'range'])
+};
+
+NumInput.defaultProps = {
+  min: 0,
+  max: 0,
+  step: 1,
+  val: 0,
+  label: '',
+  type: 'range'
+};
+
 
 export default App
